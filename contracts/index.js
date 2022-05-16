2114bb7bfe0b1ef576adbcf25b0bdc9f91560e42
Owen <oschleic@asu.edu>
2022-05-15 19:19:34 -0700

    Add and view gates

---
+++ contracts/index.js
@@ -0,0 +1,378 @@
+export const PAYMENT_RECEIVER = {
+    address: "0xC631Eb7ff97b4a44ef9baF7a0D0b94A084433A98",
+    abi:  [
+        {
+          "inputs": [
+            {
+              "internalType": "contract ISuperfluid",
+              "name": "_host",
+              "type": "address"
+            },
+            {
+              "internalType": "contract IConstantFlowAgreementV1",
+              "name": "_cfa",
+              "type": "address"
+            },
+            {
+              "internalType": "contract ISuperToken",
+              "name": "_token",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "address",
+              "name": "checkee",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "gateId",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "internalType": "int96",
+              "name": "flowRate",
+              "type": "int96"
+            },
+            {
+              "indexed": false,
+              "internalType": "contract ISuperToken",
+              "name": "token",
+              "type": "address"
+            }
+          ],
+          "name": "CheckIn",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "address",
+              "name": "checkee",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "CheckOut",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint96",
+              "name": "_flowRate",
+              "type": "uint96"
+            }
+          ],
+          "name": "addGate",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "checkIn",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "checkOut",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "checkedIn",
+          "outputs": [
+            {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "deleteGate",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "gateUsers",
+          "outputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "getAddress",
+          "outputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "getGate",
+          "outputs": [
+            {
+              "components": [
+                {
+                  "internalType": "string",
+                  "name": "name",
+                  "type": "string"
+                },
+                {
+                  "internalType": "address",
+                  "name": "payee",
+                  "type": "address"
+                },
+                {
+                  "internalType": "int96",
+                  "name": "flowRate",
+                  "type": "int96"
+                },
+                {
+                  "internalType": "contract ISuperToken",
+                  "name": "token",
+                  "type": "address"
+                },
+                {
+                  "internalType": "uint256",
+                  "name": "activeUsers",
+                  "type": "uint256"
+                }
+              ],
+              "internalType": "struct Database.Gate",
+              "name": "",
+              "type": "tuple"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "_addr",
+              "type": "address"
+            }
+          ],
+          "name": "getGateIds",
+          "outputs": [
+            {
+              "internalType": "uint256[]",
+              "name": "",
+              "type": "uint256[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            }
+          ],
+          "name": "getGateUsers",
+          "outputs": [
+            {
+              "internalType": "address[]",
+              "name": "_gateUsers",
+              "type": "address[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "_addr",
+              "type": "address"
+            }
+          ],
+          "name": "getGates",
+          "outputs": [
+            {
+              "components": [
+                {
+                  "internalType": "string",
+                  "name": "name",
+                  "type": "string"
+                },
+                {
+                  "internalType": "address",
+                  "name": "payee",
+                  "type": "address"
+                },
+                {
+                  "internalType": "int96",
+                  "name": "flowRate",
+                  "type": "int96"
+                },
+                {
+                  "internalType": "contract ISuperToken",
+                  "name": "token",
+                  "type": "address"
+                },
+                {
+                  "internalType": "uint256",
+                  "name": "activeUsers",
+                  "type": "uint256"
+                }
+              ],
+              "internalType": "struct Database.Gate[]",
+              "name": "",
+              "type": "tuple[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "_gateId",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            }
+          ],
+          "name": "renameGate",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "userIndex",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ]
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NextLink from "next/link";
+import { ethers } from "ethers";
+import { useWalletProvider } from "../context/WalletProvider";
+import { PAYMENT_RECEIVER } from "../contracts";
+
 
 const Home = () => {
+  const { walletState } = useWalletProvider();
+  const prContract = new ethers.Contract(PAYMENT_RECEIVER.address, PAYMENT_RECEIVER.abi, walletState.web3Provider);
+
+  const [gates, setGates] = useState([]);
+
+
+  const createGate = async (event) => {
+    event.preventDefault();
+    const data = {
+      name: event.target.name.value,
+      flowRate: event.target.flowRate.value,
+    }
+
+    try{
+      await prContract.connect(walletState.web3Provider.getSigner()).addGate(data.name, ethers.BigNumber.from(data.flowRate));
+      const gates = await prContract.getGates();
+      setGates(gates);
+    } catch(e) {
+      console.log('error', e);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchGates() {
+      const gates = await prContract.getGates(walletState.address);
+      setGates(gates);
+    }
+    fetchGates();
+  }, []);
+
   return (
     <>
       <div>Home</div>
-      <NextLink href="/home">Protected Page</NextLink>
+      <div>Your gates</div>
+      {gates.map(gate => (
+        <div key={gate.name}>
+          <p>Name: {gate.name}</p>
+          <p>Flow rate: {gate.flowRate.toNumber()}</p>
+        </div>
+      ))}
+
+      <form onSubmit={createGate}>
+        <label>
+          Name:
+          <input type="text" name="name" id="gate-name" required />
+        </label>
+        <label>
+          Flow rate:
+          <input type="number" name="flowRate" id="flow-rate" required/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
     </>
   );
 };
