6b239835f78911968346579dd03d89ecf0ac3276
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2022-05-17 10:43:26 +1000

    cleanupO

--- lib/gate.ts
+++ lib/gate.ts
@@ -7,6 +7,7 @@ const url =
   "https://eth-kovan.alchemyapi.io/v2/nl2PDNZm065-H3wMj2z1_mvGP81bLfqX"; 
 
 const KovanAPIURL = "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-kovan";
+// query was designed using the superfluid console
 const query = `
 query getAccounts {
     streams(where: {receiver: "0x59e30814f1d71a458e9fa6ab453f3e4cb1090e57"}) {
